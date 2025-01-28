af87d738fc3ecef1fe56d71e11786d958e298a61
VeraGerber <84279244+VeraGerber@users.noreply.github.com>
2025-01-28 21:49:43 +0300

    Add files via upload

---
+++ recipes/calculator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.conf import settings
+
+#DATA перенесена в settings.py
+
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def dish(request):
+    quantity = {}
+    name_dish = request.get_full_path().split('/')[1]
+    servings = request.GET.get('servings')
+    if not servings:
+        servings = 1
+    else:
+        servings = int(servings)
+    for ingredients in settings.DATA[name_dish]:
+        quantity[ingredients] = round(settings.DATA[name_dish][ingredients] * servings, 2)
+    context = {
+        'recipe': {name_dish: quantity}
+    }
+    print(type(request))
+    return render(request, 'calculator/index.html', context)
